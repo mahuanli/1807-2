30bf6f7d4eb600461c018504f7dfa2974f8c82f8
mahuanli <2040385352@qq.com>
2018-09-07 09:29:28 +0800

    fr

---
+++ 31day/01-机试住店.py
@@ -0,0 +1,60 @@
+import time
+import re
+list = []
+class xuanze():
+while True
+	print("1,入住")	
+	print("2,离店")
+	print("3,统计")
+	print("4,退出")
+
+
+
+class D():
+	def __init__(self,name,phone)
+		self.name = name
+		self.phone = phone
+		self.list = []
+		self.money = 0
+
+
+	def ruzhu(self.people):
+		people.time = int(input.time())
+		people.islive = True
+		people.list.append(people)
+
+	def lidian(self.name):
+		for people in self.list:
+			if people.anme == name:
+			people.islive = False
+			endtime = int(time.time())
+			deiff_money = (end)
+			print("花了%d钱")
+
+
+
+	def tongji():
+		count = 0
+		for people in self.list:
+			if not people
+
+
+
+
+w = '皇家酒店'
+while True
+	num = int(input("请选择功能:1:入住2:离店3:统计4:退出"))
+		if num == 1:
+			name = input("请输入你的姓名")
+			card = input("请输入你的身份证号")
+			d = people(name)
+			d.setCard(card)
+			D.ruzhu(d)
+
+
+
+
+
+
+
+
